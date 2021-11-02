307cb32c2faeef087c92ddaafff8dce16ab298c5
EmiKusaba <eeemi615@gmail.com>
2021-11-02 14:54:11 -0500

    added equal

--- main.js
+++ main.js
@@ -73,11 +73,11 @@ const equals = () => {
     break;
     case "subtraction": putResultInElement(subtract(firstNum, secondNum)) 
     break;
-    case "multiplication": multiply(firstNum, secondNum) 
+    case "multiplication": putResultInElement(total(firstNum, secondNum))
     break;
-    case "division": console.log(divide(firstNum, secondNum)) 
+    case "division": putResultInElemen(divide(firstNum, secondNum)) 
     break;
-    case "modulus": console.log(modulus(firstNum, secondNum)) 
+    case "modulus": putResultInElemen(modulus(firstNum, secondNum)) 
     break;
     default: "Choose an operation"
   }
